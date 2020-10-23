36b07d1c8e2c31eea4e623fdc58c0b2989f3beb1
Nidhi Soni <nsoni52@gmail.com>
2020-10-23 16:40:04 +0530

    signup page

---
+++ pages/signup.js
@@ -0,0 +1,17 @@
+
+import {Layout} from "../components/common/Layout" 
+import Signup from "../components/Auth/Register"
+export default function AboutUs() {
+  
+  const meta = {
+    title:"Sign up to connect with buyers and sellers of unique, sustainable, recycled, organic products | Qalara",
+    description:"Join the community of buyers and sellers on Qalara, the best way to trade Indian products, home and kitchen items, handmade products, carpets and textiles.",
+}
+  
+  return (
+    <Layout meta={meta}>
+      <Signup/>
+    </Layout>  
+  )
+
+}
