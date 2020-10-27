8e2720fe031b2dde158fff885085bac875f607db
Manjunath Bukya <manjunath.b@acuverconsulting.com>
2020-10-27 15:20:49 +0000

    Update _app.js. ssr: true

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,14 @@ import Cookies from 'js-cookie'
 import store  from '../store';
 import { setTokenSuccess, setTokenFail } from '../store/actions';
 import {useRouter} from 'next/router';
+import { Amplify } from 'aws-amplify';
+import awsExports from '../src/aws-exports';
+
+Amplify.configure({
+  ...awsExports,
+  ssr: true
+}
+)
 
 let timeSkew = 10;
 const generateToken=async ()=>{
