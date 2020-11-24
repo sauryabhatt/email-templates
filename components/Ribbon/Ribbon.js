76d7bc0d848b03b4181ddbf2dc1120a3b504f482
nandini.kj <nandini.kj@oracle.com>
2020-11-24 15:50:05 +0530

    Bug fixes

--- components/Ribbon/Ribbon.js
+++ components/Ribbon/Ribbon.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import Slider from "react-slick";
-import ribbonCross from "../../public/filestore/RibbonCross"
+/** @format */
 
-export default function Ribbon(props){
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import Link from "next/link";
+import ribbonCross from "../../public/filestore/RibbonCross";
+
+export default function Ribbon(props) {
   const settings = {
     infinite: true,
     speed: 2000,
@@ -17,26 +20,55 @@ export default function Ribbon(props){
     autoplay: true,
     autoplaySpeed: 7000,
   };
-  let slider = ""
-  let t = ["$75 OFF on shipping for all orders", "ZERO Commissions. FREE quality inspections"]
-  return(
-    <div  className="home-page-ribben-wrp">
+  let slider = "";
+  let t = [
+    "$75 OFF on shipping for all orders",
+    "ZERO Commissions. FREE quality inspections",
+  ];
+
+  const [pathname, setPathname] = useState("");
+
+  useEffect(() => {
+    let { location: { pathname = "" } = {} } = window || {};
+    setPathname(pathname);
+  }, []);
+
+  return (
+    <div className="home-page-ribben-wrp">
       <Slider ref={(c) => (slider = c)} {...settings}>
-        {t.map(a => {
-          return(
-            <a target="_blank" href="/promotionsFAQ" key = {a} className="home-page-ribben">
+        {t.map((a) => {
+          return (
+            <a
+              target="_blank"
+              href="/promotionsFAQ"
+              key={a}
+              className="home-page-ribben"
+            >
               <span className="ribbon-title">
                 {a}.
-                <a href="/" className="ribbon-link">T&C*</a>
+                {pathname !== "/" ? (
+                  <Link href="/">
+                    <span className="ribbon-link qa-cursor">T&C*</span>
+                  </Link>
+                ) : (
+                  <span
+                    className="ribbon-link qa-cursor"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                  >
+                    T&C*
+                  </span>
+                )}
               </span>
             </a>
-          )
-        })
-
-        }
+          );
+        })}
       </Slider>
-      <span onClick={()=>props.setShowRibbon(false)} className = "cross-btn">{ribbonCross()}</span>
-    </div >
-
-  )
+      <span onClick={() => props.setShowRibbon(false)} className="cross-btn">
+        {ribbonCross()}
+      </span>
+    </div>
+  );
 }
