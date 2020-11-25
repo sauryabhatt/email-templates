dd61f9233cd4198f8ae759a03c235a960ae13980
nandini.kj <nandini.kj@oracle.com>
2020-11-25 11:36:25 +0530

    schedule video demo

--- components/mobile/SellerLandingMobile.js
+++ components/mobile/SellerLandingMobile.js
@@ -612,7 +612,7 @@ function SellerLandingMobile(props) {
       setShowScheduleBenefits(false);
       setShowScheduling(true);
     } else {
-      loginToApp(keycloak, { currentPath: router.pathname });
+      loginToApp(keycloak, { currentPath: `/seller/${router.query.sellerId}` });
     }
   };
 
