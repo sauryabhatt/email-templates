72e29dbdbde1501355e71c1c85d1f679f2547901
nandini.kj <nandini.kj@oracle.com>
2021-01-09 10:07:24 +0530

    Initial props change

--- pages/_app.js
+++ pages/_app.js
@@ -73,13 +73,13 @@ function parseCookies(req) {
   return cookie.parse(req.headers.cookie || "");
 }
 
-// MyApp.getInitialProps = async ({ ctx }) => {
-//   const { req, res } = ctx || {};
+MyApp.getInitialProps = async ({ ctx }) => {
+  const { req, res } = ctx || {};
 
-//   return {
-//     cookies: parseCookies(req),
-//   };
-// };
+  return {
+    cookies: parseCookies(req),
+  };
+};
 
 export default MyApp;
 
