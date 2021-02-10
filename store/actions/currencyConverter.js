721c094e262356fa264d2c222f43000b6a9b0a6d
nandini.kj <nandini.kj@oracle.com>
2021-02-10 15:33:01 +0530

    Currency conversion API implementation

--- store/actions/currencyConverter.js
+++ store/actions/currencyConverter.js
@@ -10,7 +10,6 @@ export const setCurrency = (currencies, rates) => {
 };
 
 export const setCurrencyFormat = (convertToCurrency) => {
-  console.log(convertToCurrency);
   return {
     type: actionTypes.GET_CURRENCY_CONVERSION_FORMAT,
     payload: { convertToCurrency },
@@ -26,18 +25,25 @@ export const getCurrentFormat = (convertToCurrency) => {
 export const getCurrencyConversion = (base, callback = "") => async (
   dispatch
 ) => {
-  let url = `https://api.exchangeratesapi.io/latest?base=${base}`;
+  // let url = `https://api.exchangeratesapi.io/latest?base=${base}`;
+  let url = `${process.env.NEXT_PUBLIC_REACT_APP_API_FORM_URL}/currencies/conversion-rates?base=${base}`;
+
   fetch(url, {
     method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.NEXT_PUBLIC_ANONYMOUS_TOKEN,
+    },
   })
     .then((res) => {
       if (res.ok) {
         return res.json();
       } else {
-        throw res.statusText || "Error while saving user deatils.";
+        throw new Error("Something went wrong");
       }
     })
     .then((res) => {
+      console.log("API Resp 1 ", res);
       let currencies = res["rates"];
       let rates = Object.keys(res["rates"]).sort();
       if (callback) {
@@ -47,6 +53,29 @@ export const getCurrencyConversion = (base, callback = "") => async (
       }
     })
     .catch((err) => {
-      // console.log(err.message);
+      // console.log(err);
+      fetch(`https://api.exchangeratesapi.io/latest?base=${base}`, {
+        method: "GET",
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error("Something went wrong");
+          }
+        })
+        .then((res) => {
+          console.log("API Resp 2 ", res);
+          let currencies = res["rates"];
+          let rates = Object.keys(res["rates"]).sort();
+          if (callback) {
+            callback(res);
+          } else {
+            return dispatch(setCurrency(rates, currencies));
+          }
+        })
+        .catch((err) => {
+          // console.log(err.message);
+        });
     });
 };
