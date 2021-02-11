dc3d525154169d97754c43c346127cbcfcd14d42
nandini.kj <nandini.kj@oracle.com>
2021-02-11 11:14:46 +0530

    Currency converter api

--- store/actions/currencyConverter.js
+++ store/actions/currencyConverter.js
@@ -25,15 +25,15 @@ export const getCurrentFormat = (convertToCurrency) => {
 export const getCurrencyConversion = (base, callback = "") => async (
   dispatch
 ) => {
-  // let url = `https://api.exchangeratesapi.io/latest?base=${base}`;
-  let url = `${process.env.NEXT_PUBLIC_REACT_APP_API_FORM_URL}/currencies/conversion-rates?base=${base}`;
+  let url = `https://api.exchangeratesapi.io/latest?base=${base}`;
+  // let url = `${process.env.NEXT_PUBLIC_REACT_APP_API_FORM_URL}/currencies/conversion-rates?base=${base}`;
 
   fetch(url, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + process.env.NEXT_PUBLIC_ANONYMOUS_TOKEN,
-    },
+    // headers: {
+    //   "Content-Type": "application/json",
+    //   Authorization: "Bearer " + process.env.NEXT_PUBLIC_ANONYMOUS_TOKEN,
+    // },
   })
     .then((res) => {
       if (res.ok) {
@@ -54,28 +54,28 @@ export const getCurrencyConversion = (base, callback = "") => async (
     })
     .catch((err) => {
       // console.log(err);
-      fetch(`https://api.exchangeratesapi.io/latest?base=${base}`, {
-        method: "GET",
-      })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            throw new Error("Something went wrong");
-          }
-        })
-        .then((res) => {
-          console.log("API Resp 2 ", res);
-          let currencies = res["rates"];
-          let rates = Object.keys(res["rates"]).sort();
-          if (callback) {
-            callback(res);
-          } else {
-            return dispatch(setCurrency(rates, currencies));
-          }
-        })
-        .catch((err) => {
-          // console.log(err.message);
-        });
+      // fetch(`https://api.exchangeratesapi.io/latest?base=${base}`, {
+      //   method: "GET",
+      // })
+      //   .then((res) => {
+      //     if (res.ok) {
+      //       return res.json();
+      //     } else {
+      //       throw new Error("Something went wrong");
+      //     }
+      //   })
+      //   .then((res) => {
+      //     console.log("API Resp 2 ", res);
+      //     let currencies = res["rates"];
+      //     let rates = Object.keys(res["rates"]).sort();
+      //     if (callback) {
+      //       callback(res);
+      //     } else {
+      //       return dispatch(setCurrency(rates, currencies));
+      //     }
+      //   })
+      //   .catch((err) => {
+      //     // console.log(err.message);
+      //   });
     });
 };
