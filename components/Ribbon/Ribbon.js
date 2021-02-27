eca1f3a57cdedf7ef0d37c3d562474de7ae4a6d4
nandini.kj <nandini.kj@oracle.com>
2021-02-27 08:21:59 +0530

    Home page chnages

--- components/Ribbon/Ribbon.js
+++ components/Ribbon/Ribbon.js
@@ -22,8 +22,8 @@ export default function Ribbon(props) {
   };
   let slider = "";
   let t = [
+    "USD 100 OFF on your first order. Minimum order value USD 500. Apply code FAB100",
     "FREE SHIPPING on select styles",
-    // "Holiday offer! 5% off on all orders",
     "ZERO Commissions. FREE quality inspections",
   ];
 
@@ -41,7 +41,7 @@ export default function Ribbon(props) {
           return (
             <a
               target="_blank"
-              href={i === 0 ? "/gb/?f_isfreeshipping=true" : "/promotionsFAQ"}
+              href={i === 1 ? "/gb/?f_isfreeshipping=true" : "/promotionsFAQ"}
               key={a}
               className="home-page-ribben"
             >
