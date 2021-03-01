603b38729c1ce796d7b562086d8f6da940c98ba5
nandini.kj <nandini.kj@oracle.com>
2021-03-01 22:24:57 +0530

    Uncommenting tawk.io

--- pages/_app.js
+++ pages/_app.js
@@ -32,19 +32,19 @@ function MyApp({ Component, pageProps, cookies }) {
     TagManager.initialize({ gtmId: "GTM-KTVSR8R" });
   }, []);
 
-  // useEffect(() => {
-  //   let Tawk_API = Tawk_API || {},
-  //     Tawk_LoadStart = new Date();
-  //   (function () {
-  //     let s1 = document.createElement("script"),
-  //       s0 = document.getElementsByTagName("script")[0];
-  //     s1.async = true;
-  //     s1.src = "https://embed.tawk.to/5fdb39afdf060f156a8df4ae/1epo5ilog";
-  //     s1.charset = "UTF-8";
-  //     s1.setAttribute("crossorigin", "*");
-  //     s0.parentNode.insertBefore(s1, s0);
-  //   })();
-  // }, []);
+  useEffect(() => {
+    let Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      let s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/5fdb39afdf060f156a8df4ae/1epo5ilog";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
 
   return (
     <SSRKeycloakProvider
