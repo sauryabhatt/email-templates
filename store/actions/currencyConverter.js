6bbed9ea504f20a551ed4e67b18a48c2d871d78a
nandini <k.j.nandu92@gmail.com>
2021-04-09 12:47:48 +0530

    Custom image changes

--- store/actions/currencyConverter.js
+++ store/actions/currencyConverter.js
@@ -12,7 +12,6 @@ export const setCurrency = (currencies, rates) => {
 };
 
 export const setCurrencyFormat = (convertToCurrency) => {
-  console.log(convertToCurrency);
   return {
     type: actionTypes.GET_CURRENCY_CONVERSION_FORMAT,
     payload: { convertToCurrency },
@@ -28,47 +27,110 @@ export const getCurrentFormat = (convertToCurrency) => {
 export const getCurrencyConversion = (base, callback = "") => async (
   dispatch
 ) => {
-  let url = `https://api.exchangeratesapi.io/v1/latest?base=${base}&access_key=${ACCESS_KEY_CURRENCY}`;
-  fetch(url, {
-    method: "GET",
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw res.statusText || "Error while saving user deatils.";
-      }
-    })
-    .then((res) => {
-      let fallbackData = {
-        USD: 1,
-      };
-      let currencies;
-      if (res["success"] === true) {
-        currencies = res["rates"];
-      } else {
-        currencies = fallbackData;
-      }
+  let url = `${process.env.NEXT_PUBLIC_REACT_APP_API_FORM_URL}/currencies/conversion-rates?base=${base}`;
 
+  let hours = 6;
+  let saved = sessionStorage.getItem("currencySaved");
+  let currencyDetails = sessionStorage.getItem("currencyDetails");
+  let retryCount = 0;
+
+  const getExchangeRateDetails = () => {
+    let apiURL = `https://api.exchangeratesapi.io/v1/latest?base=${base}&access_key=${ACCESS_KEY_CURRENCY}`;
+    fetch(apiURL, {
+      method: "GET",
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw res.statusText || "Error while saving user deatils.";
+        }
+      })
+      .then((res) => {
+        let fallbackData = {
+          USD: 1,
+        };
+        let currencies;
+        if (res["success"] === true) {
+          currencies = res["rates"];
+        } else {
+          currencies = fallbackData;
+        }
+
+        let rates = Object.keys(currencies).sort();
+        if (callback) {
+          callback(res);
+        } else {
+          return dispatch(setCurrency(rates, currencies));
+        }
+      })
+      .catch((err) => {
+        let obj = sessionStorage.getItem("currencyDetails") || {};
+        obj = JSON.parse(obj);
+        let currencies = obj["rates"] || { USD: 1.0 };
+        let rates = Object.keys(currencies).sort();
+        return dispatch(setCurrency(rates, currencies));
+      });
+  };
+
+  const getCurrencyDetails = () => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.NEXT_PUBLIC_ANONYMOUS_TOKEN,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((res) => {
+        sessionStorage.setItem("currencySaved", new Date().getTime());
+        let currencies = res["rates"] || { USD: 1.0 };
+        let rates = Object.keys(currencies).sort();
+        let obj = {};
+        obj["rates"] = currencies;
+        if (callback) {
+          callback(obj);
+        }
+        sessionStorage.setItem("currencyDetails", JSON.stringify(obj));
+        return dispatch(setCurrency(rates, currencies));
+      })
+      .catch((err) => {
+        if (retryCount < 3) {
+          getCurrencyDetails();
+        } else {
+          getExchangeRateDetails();
+        }
+        retryCount++;
+      });
+  };
+
+  if (
+    saved &&
+    currencyDetails &&
+    new Date().getTime() - saved > hours * 60 * 60 * 1000
+  ) {
+    getCurrencyDetails();
+  } else {
+    if (
+      sessionStorage.getItem("currencyDetails") &&
+      sessionStorage.getItem("currencySaved")
+    ) {
+      let obj = sessionStorage.getItem("currencyDetails") || {};
+      obj = JSON.parse(obj);
+      if (callback) {
+        callback(obj);
+      }
+      let currencies = obj["rates"] || { USD: 1.0 };
       let rates = Object.keys(currencies).sort();
-      if (callback) {
-        callback(res);
-      } else {
-        return dispatch(setCurrency(rates, currencies));
-      }
-    })
-    .catch((err) => {
-      let fallbackData = {
-        USD: 1,
-      };
-      let currencies = fallbackData;
-
-      let rates = Object.keys(fallbackData).sort();
-      if (callback) {
-        callback(res);
-      } else {
-        return dispatch(setCurrency(rates, currencies));
-      }
-      // console.log(err.message);
-    });
+      return dispatch(setCurrency(rates, currencies));
+    } else {
+      getCurrencyDetails();
+    }
+  }
 };
